b8b302e9816594bc903b2baf1ab0787fb8694d98
Foogg5776 <vcoj05508@gmail.com>
2023-04-06 18:16:14 +1000

    fsfdfd

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let ad = 11
+let age = prompt("awadasdasda ну ты понял")
+if (age <= 10){
+    console.log("пон");
+}
+if (age <= 20 && age >10){
+    console.log("tampon");
+}
+if (age > 20){
+    console.log("ну ладно");
+}
+
+
+console.log("здарова пивас", 1 + ad); 
